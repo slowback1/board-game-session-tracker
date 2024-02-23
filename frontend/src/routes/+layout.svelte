2eaa4e5d4ea64458bcfb26b8fb8185b4ec66bbaa
Andrew Wobeck <dwobeck@gmail.com>
2024-02-23 15:14:40 -0600

    Add a wait for config to load before loading the rest of the app, so that things don't b reak that rely on the config

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,46 +1,50 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import MessageBus from '$lib/bus/MessageBus';
-	import getRealStorageProvider from '$lib/bus/realStorageProvider';
-	import Header from '$lib/components/navigation/Header.svelte';
-	import UrlPathProvider, { RealUrlProvider } from '$lib/providers/urlPathProvider';
-	import { ColorTheme } from '$lib/services/ThemeService';
-	import { Messages } from '$lib/bus/Messages';
-	import ConfigService, { type ApplicationConfig } from '$lib/services/ConfigService';
+    import {onMount} from 'svelte';
+    import MessageBus from '$lib/bus/MessageBus';
+    import getRealStorageProvider from '$lib/bus/realStorageProvider';
+    import Header from '$lib/components/navigation/Header.svelte';
+    import UrlPathProvider, {RealUrlProvider} from '$lib/providers/urlPathProvider';
+    import {ColorTheme} from '$lib/services/ThemeService';
+    import {Messages} from '$lib/bus/Messages';
+    import ConfigService, {type ApplicationConfig} from '$lib/services/ConfigService';
 
-	let currentTheme: ColorTheme = ColorTheme.Light;
+    let currentTheme: ColorTheme = ColorTheme.Light;
 
-	onMount(() => {
-		MessageBus.initialize(getRealStorageProvider());
-		UrlPathProvider.initialize(new RealUrlProvider());
-		ConfigService.initialize();
+    let configHasLoaded = false;
 
-		MessageBus.subscribe<ColorTheme>(
-			Messages.CurrentTheme,
-			(value) => (currentTheme = value ?? ColorTheme.Light)
-		);
-	});
+    onMount(async () => {
+        MessageBus.initialize(getRealStorageProvider());
+        UrlPathProvider.initialize(new RealUrlProvider());
+        await ConfigService.initialize();
+        configHasLoaded = true;
+        MessageBus.subscribe<ColorTheme>(
+            Messages.CurrentTheme,
+            (value) => (currentTheme = value ?? ColorTheme.Light)
+        );
+    });
 </script>
 
 <div
-	class:light-theme={currentTheme === ColorTheme.Light}
-	class:dark-theme={currentTheme === ColorTheme.Dark}
+        class:light-theme={currentTheme === ColorTheme.Light}
+        class:dark-theme={currentTheme === ColorTheme.Dark}
 >
-	<Header />
-	<main id="content" class="main-content">
-		<slot />
-	</main>
+    {#if configHasLoaded}
+        <Header/>
+        <main id="content" class="main-content">
+            <slot/>
+        </main>
+    {/if}
 </div>
 
 <style global>
-	@import '../style/reset.css';
-	@import '../style/globals.css';
+    @import '../style/reset.css';
+    @import '../style/globals.css';
 
-	.main-content {
-		min-height: calc(100vh - var(--gutters-y) * 2 - var(--header-height));
-		padding: var(--gutters-y) var(--gutters-x);
-		scroll-behavior: auto;
-		display: flex;
-		flex-direction: column;
-	}
+    .main-content {
+        min-height: calc(100vh - var(--gutters-y) * 2 - var(--header-height));
+        padding: var(--gutters-y) var(--gutters-x);
+        scroll-behavior: auto;
+        display: flex;
+        flex-direction: column;
+    }
 </style>
