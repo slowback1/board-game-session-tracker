165a88fbb1114d3b55eb1b2f4f2e24d89bc3b580
Andrew Wobeck <dwobeck@gmail.com>
2024-03-03 11:20:25 -0600

    Style up the inventory type form

--- frontend/src/lib/partials/inventoryTypes/InventoryTypeForm.svelte
+++ frontend/src/lib/partials/inventoryTypes/InventoryTypeForm.svelte
@@ -3,6 +3,7 @@
     import TextBox from "$lib/ui/inputs/TextBox/TextBox.svelte";
     import Button from "$lib/ui/buttons/Button/Button.svelte";
     import type {InventoryTypeOption, InventoryTypeResponse} from "$lib/api/inventoryTypeApi";
+    import TrashIcon from "$lib/ui/icons/TrashIcon.svelte";
 
     export let errors: string[] = [];
 
@@ -36,18 +37,63 @@
             bind:value={name}
     />
 
-    {#each options as option, index}
-        <TextBox bind:value={option.label} label="Label" id={`inventory-type-form__option-${index}-label`}/>
-        <TextBox bind:value={option.value} label="Value" id={`inventory-type-form__option-${index}-value`}/>
-        <Button disabled={options.length === 1 }
-                onClick={() => onRemoveOptionClick(index)}
-                testId={`inventory-type-form__remove-option-${index}`}>
-            Remove Option
-        </Button>
-    {/each}
+    <div class="inventory-type-form__option-list">
+        {#each options as option, index}
+            <div class="inventory-type-form__option">
+                <TextBox bind:value={option.label} label="Label" id={`inventory-type-form__option-${index}-label`}/>
+                <TextBox bind:value={option.value} label="Value" id={`inventory-type-form__option-${index}-value`}/>
+                <Button disabled={options.length === 1 }
+                        onClick={() => onRemoveOptionClick(index)}
+                        testId={`inventory-type-form__remove-option-${index}`}
+                        size="small"
+                        variant="text"
+                >
+                    <TrashIcon/>
+                </Button>
+            </div>
+        {/each}
 
-    <Button onClick={onAddOptionClick} testId="inventory-type-form__add-option">Add Option</Button>
+        <Button size="small"
+                variant="primary"
+                onClick={onAddOptionClick}
+                testId="inventory-type-form__add-option"
+        >
+            Add Option
+        </Button>
+    </div>
 
     <Button testId="inventory-type-form__submit">Submit</Button>
 
 </FormWrapper>
+
+<style global>
+    .inventory-type-form__option-list {
+        display: flex;
+        flex-direction: column;
+        align-items: flex-end;
+        gap: 8px;
+        margin-bottom: 20px;
+    }
+
+    .inventory-type-form__option {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        gap: 12px;
+    }
+
+    @media screen and (max-width: 560px) {
+        .inventory-type-form__option {
+            flex-direction: column;
+            border: 1px solid;
+            padding: 4px;
+        }
+    }
+
+
+    :global(.icon) {
+        fill: color-mix(in lab, currentColor, 80% var(--color-error-font));
+        width: var(--font-size-small);
+        height: var(--font-size-small);
+    }
+</style>
