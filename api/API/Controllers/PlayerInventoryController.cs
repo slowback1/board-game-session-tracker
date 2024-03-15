cec4b9771171d3a7847bdaeb9168a0b5cbdd15e6
Andrew Wobeck <dwobeck@gmail.com>
2024-03-14 22:19:05 -0500

    Make a controller for player inventory, and refactor bruno files to use variables

---
+++ api/API/Controllers/PlayerInventoryController.cs
@@ -0,0 +1,41 @@
+﻿using API.Attributes;
+using Database.Common.DTOs;
+using Database.Common.Game;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Authenticated]
+[Route("PlayerInventory")]
+public class PlayerInventoryController : BaseController
+{
+    private readonly PlayerInventoryRepository _repository;
+
+    public PlayerInventoryController(IConfiguration config) : base(config)
+    {
+        _repository = new PlayerInventoryRepository(Storer);
+    }
+
+    [HttpGet]
+    [Route("{gameId}")]
+    public async Task<ApiResponse<List<PlayerInventory>>> GetPlayerInventoryForGame(string gameId)
+    {
+        return await _repository.GetPlayerInventoryForGame(gameId);
+    }
+
+    [HttpPut]
+    [Route("{gameId}/SinglePlayer")]
+    public async Task<ApiResponse<PlayerInventoryItem>> UpdateSinglePlayerInventory(string gameId,
+        [FromBody] UpdatePlayerInventoryRequest request)
+    {
+        return await _repository.UpdatePlayerInventoryItem(gameId, request);
+    }
+
+    [HttpPut]
+    [Route("{gameId}/AllPlayers")]
+    public async Task<ApiResponse<List<PlayerInventoryItem>>> UpdateAllPlayerInventories(string gameId,
+        [FromBody] UpdateAllPlayerInventoriesRequest request)
+    {
+        return await _repository.UpdateAllPlayersInventories(gameId, request);
+    }
+}
